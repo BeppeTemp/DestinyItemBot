0b27ba2c984911b917fb500b5fcae079f5a7cc95
Giuseppe Arienzo <55873294+BeppeTemp@users.noreply.github.com>
2021-01-19 10:45:34 +0100

    Abemus armaiolo

--- test-request.py
+++ test-request.py
@@ -13,27 +13,42 @@ state = "state=6i0mkLx79Hp91nzWVeHrzHG4"
 requests.get(base_oauth_path+respone_type+client_id+redirect_uri+state)
 """
 
-"""
+
 payload = (('client_id', '35042'),
            ('grant_type', 'authorization_code'), 
-           ('code', '995b2e707c00e4a7cca7dbe51bd311ac'))
+           ('code', 'b5262a8c2e8302f1b250ff90fa40aa29'))
 
 r = requests.post('https://www.bungie.net/platform/app/oauth/token/', data=payload)
 print(r.text)
-"""
 
+
+"""
 HEADERS = {"X-API-Key":'f607ba1906f849f09040a3c4a87e0d32'}
 
-r = requests.get("http://www.bungie.net/Platform/Destiny/1/Account/4392828/Summary/", headers=HEADERS)
+r = requests.get("http://www.bungie.net/Platform//User/GetMembershipsById/4392828/1/", headers=HEADERS)
 print(r.text)
+"""
 
 """
 HEADERS = {"X-API-Key":'f607ba1906f849f09040a3c4a87e0d32'}
 
-r = requests.get("https://www.bungie.net/Platform//Destiny2/1/Profile/4392828/Character/{characterId}/Vendors/672118013/", headers=HEADERS)
+r = requests.get("http://www.bungie.net/Platform/Destiny2/1/Profile/4611686018429759726/?components=200", headers=HEADERS)
+print(r.text)
+"""
+
+"""
+HEADERS = {
+    "X-API-Key":'f607ba1906f849f09040a3c4a87e0d32',
+    "Authorization" : 'CM7vAhKGAgAg16cxKRsOm0DFn2yDWdNQXxSQbJ0vDDTivxWi+c/yuqvgAAAA/a8gIFs/hdV86A0wRNH4gLRcBmTv3EXOmuUE7OXdNSVaO3f9eG+VRHX5DK4LSAlatsOzYbnhjC21HdzM14G4E+Igo9o2hnzPLop7GTelIzuZqZA8+YpDHGt41hJGnXfw3EjOdLUCM3+Yzdtt/vcv5dmcfK7Uux4C5ctqY1MO5rv52WqNIp7/VwF7rs0gv8AM57SfuuqkzHdRWz/jqTYhLUKaDCJMGPfTNUPf6hRWYbpj5Hx7DxDuVQOo4fVaDEAwdyUzlDZgoiVus7bT+K3rATJE+xqm1IrLYCtE+bOfciY='
+}
+
+r = requests.get("https://www.bungie.net/Platform/Destiny2/1/Profile/4392828/Character/2305843009278650655/Vendors/672118013/?components=400", headers=HEADERS)
 print(r.text)
 """
 
 #Scrittura della risposta ottenuta in un file json
 with open("response.json","w") as file:
     json.dump(r.json(),file)
+
+
+
---
+++ vendor_request.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+HEADERS = {
+    "X-API-Key":'f607ba1906f849f09040a3c4a87e0d32',
+    "Authorization" : 'Bearer CMXvAhKGAgAgHGOrdgdVJHkvE9388mAeNtRsidCE4zm/2vZHBTLp2CHgAAAAyqmjzf5bARfzJJkte5z2cUBTiZy9cCLBJ5CKriF/ZusfZFJpzf7Yl7T2L5544gbiBzeVyoTp+mmdwQOco4xZjfeuv74EV7U9pThMj/LCyaHMbpcC28Px9Q4TuJNFHroXyVsmKYFvgGPlTyjw+3aZRzcOH6+F0QGEAsom83Xe6TqLnt4iCNwHKEn8yrP4hTFQvL2VFzK/Lswtf5YUoYHh3dHnab0Uny84oyNy10AlJcyTEiFIU68G4l/XEVl7DaN0BlNgcql9Mtg3G4At84WIdKTmlybJg3dt7NzHZVYI8gk='
+}
+
+r = requests.get("https://www.bungie.net/Platform/Destiny2/1/Profile/4611686018429759726/Character/2305843009278650655/Vendors/672118013/?components=400,401,402", headers=HEADERS)
+print(r.text)
+
+
+#Scrittura della risposta ottenuta in un file json
+with open("vendor.json","w") as file:
+    json.dump(r.json(),file)
