de65acd1ef57b2121945649c39ba3e3942585a6a
Giuseppe Arienzo <55873294+BeppeTemp@users.noreply.github.com>
2021-01-19 10:50:09 +0100

    Aggiunta commenti

--- test-request.py
+++ test-request.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 #https://www.bungie.net/it/oauth/authorize?response_type=code&client_id=35042&redirect_uri=https://destinyitembot/callback&state=6i0mkLx79Hp91nzWVeHrzHG4
-
+#Chiamata per ottenere il codice da inviar eper ottenere l'access token
 """
 base_oauth_path = "https://www.bungie.net/en/oauth/authorize?"
 respone_type = "response_type=code&"
@@ -12,37 +12,27 @@ state = "state=6i0mkLx79Hp91nzWVeHrzHG4"
 
 requests.get(base_oauth_path+respone_type+client_id+redirect_uri+state)
 """
-
-
+#Chiamata per ottenere l'access token e l'id
+"""
 payload = (('client_id', '35042'),
            ('grant_type', 'authorization_code'), 
            ('code', 'b5262a8c2e8302f1b250ff90fa40aa29'))
 
 r = requests.post('https://www.bungie.net/platform/app/oauth/token/', data=payload)
 print(r.text)
-
-
+"""
+#Chiamata per ottenere l'id di una determinata piattaforma
 """
 HEADERS = {"X-API-Key":'f607ba1906f849f09040a3c4a87e0d32'}
 
 r = requests.get("http://www.bungie.net/Platform//User/GetMembershipsById/4392828/1/", headers=HEADERS)
 print(r.text)
 """
-
+#Chiamata per ottenere l'id di un personaggio
 """
 HEADERS = {"X-API-Key":'f607ba1906f849f09040a3c4a87e0d32'}
 
 r = requests.get("http://www.bungie.net/Platform/Destiny2/1/Profile/4611686018429759726/?components=200", headers=HEADERS)
-print(r.text)
-"""
-
-"""
-HEADERS = {
-    "X-API-Key":'f607ba1906f849f09040a3c4a87e0d32',
-    "Authorization" : 'CM7vAhKGAgAg16cxKRsOm0DFn2yDWdNQXxSQbJ0vDDTivxWi+c/yuqvgAAAA/a8gIFs/hdV86A0wRNH4gLRcBmTv3EXOmuUE7OXdNSVaO3f9eG+VRHX5DK4LSAlatsOzYbnhjC21HdzM14G4E+Igo9o2hnzPLop7GTelIzuZqZA8+YpDHGt41hJGnXfw3EjOdLUCM3+Yzdtt/vcv5dmcfK7Uux4C5ctqY1MO5rv52WqNIp7/VwF7rs0gv8AM57SfuuqkzHdRWz/jqTYhLUKaDCJMGPfTNUPf6hRWYbpj5Hx7DxDuVQOo4fVaDEAwdyUzlDZgoiVus7bT+K3rATJE+xqm1IrLYCtE+bOfciY='
-}
-
-r = requests.get("https://www.bungie.net/Platform/Destiny2/1/Profile/4392828/Character/2305843009278650655/Vendors/672118013/?components=400", headers=HEADERS)
 print(r.text)
 """
 
--- vendor_request.py
+++ vendor_request.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+#interrogazione per ottenere le componenti 400, 401, 402 dell'armaiolo
 HEADERS = {
     "X-API-Key":'f607ba1906f849f09040a3c4a87e0d32',
     "Authorization" : 'Bearer CMXvAhKGAgAgHGOrdgdVJHkvE9388mAeNtRsidCE4zm/2vZHBTLp2CHgAAAAyqmjzf5bARfzJJkte5z2cUBTiZy9cCLBJ5CKriF/ZusfZFJpzf7Yl7T2L5544gbiBzeVyoTp+mmdwQOco4xZjfeuv74EV7U9pThMj/LCyaHMbpcC28Px9Q4TuJNFHroXyVsmKYFvgGPlTyjw+3aZRzcOH6+F0QGEAsom83Xe6TqLnt4iCNwHKEn8yrP4hTFQvL2VFzK/Lswtf5YUoYHh3dHnab0Uny84oyNy10AlJcyTEiFIU68G4l/XEVl7DaN0BlNgcql9Mtg3G4At84WIdKTmlybJg3dt7NzHZVYI8gk='
