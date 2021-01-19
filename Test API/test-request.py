090254bfdd6ec37b889726443111274c3ebe0ddd
Giuseppe Arienzo <55873294+BeppeTemp@users.noreply.github.com>
2021-01-19 15:38:26 +0100

    Caricamento bot template

--- Test API/test-request.py
+++ Test API/test-request.py
@@ -16,7 +16,7 @@ requests.get(base_oauth_path+respone_type+client_id+redirect_uri+state)
 """
 payload = (('client_id', '35042'),
            ('grant_type', 'authorization_code'), 
-           ('code', 'b5262a8c2e8302f1b250ff90fa40aa29'))
+           ('code', '595b03720e8efd7c49ce4a495170e266'))
 
 r = requests.post('https://www.bungie.net/platform/app/oauth/token/', data=payload)
 print(r.text)
@@ -29,12 +29,12 @@ r = requests.get("http://www.bungie.net/Platform//User/GetMembershipsById/439282
 print(r.text)
 """
 #Chiamata per ottenere l'id di un personaggio
-"""
+
 HEADERS = {"X-API-Key":'f607ba1906f849f09040a3c4a87e0d32'}
 
 r = requests.get("http://www.bungie.net/Platform/Destiny2/1/Profile/4611686018429759726/?components=200", headers=HEADERS)
 print(r.text)
-"""
+
 
 #Scrittura della risposta ottenuta in un file json
 with open("response.json","w") as file:
--- Test API/vendor_request.py
+++ Test API/vendor_request.py
@@ -4,7 +4,7 @@ import json
 #interrogazione per ottenere le componenti 400, 401, 402 dell'armaiolo
 HEADERS = {
     "X-API-Key":'f607ba1906f849f09040a3c4a87e0d32',
-    "Authorization" : 'Bearer CMXvAhKGAgAgHGOrdgdVJHkvE9388mAeNtRsidCE4zm/2vZHBTLp2CHgAAAAyqmjzf5bARfzJJkte5z2cUBTiZy9cCLBJ5CKriF/ZusfZFJpzf7Yl7T2L5544gbiBzeVyoTp+mmdwQOco4xZjfeuv74EV7U9pThMj/LCyaHMbpcC28Px9Q4TuJNFHroXyVsmKYFvgGPlTyjw+3aZRzcOH6+F0QGEAsom83Xe6TqLnt4iCNwHKEn8yrP4hTFQvL2VFzK/Lswtf5YUoYHh3dHnab0Uny84oyNy10AlJcyTEiFIU68G4l/XEVl7DaN0BlNgcql9Mtg3G4At84WIdKTmlybJg3dt7NzHZVYI8gk='
+    "Authorization" : 'Bearer CLbvAhKGAgAgTLtk3K/tJKojdfkNHX1yvnw5a+hW0v6yb4V7DgrgqJPgAAAAXx2iPFjQS3ewnObn2ZDSlcwTYXVeKcGQcMCb3ymFIC2ei/bcMZzOhG2j6NTldIvPTMpvtDgCPUFyvp5paRKOSWGG5EvSS68NRcGrnf+psan/sNwJQ0z9Un/Ivg29Ct9DYEfuaMFubzuykbzTmV5zKYaXaSKr0G/F9XhIROOU4uLBBBlZgNcVZfbYwwdMdbXcw7dW7qDSUl2yCQT1NtWn/j7xNC56jxYpuU45Cvfwvud//janQBY5uIyXWD/HeMSjMFaMcyaOnWC91OvW00lNGrAwD61wljBGeuen47m9vjI='
 }
 
 r = requests.get("https://www.bungie.net/Platform/Destiny2/1/Profile/4611686018429759726/Character/2305843009278650655/Vendors/672118013/?components=400,401,402", headers=HEADERS)
