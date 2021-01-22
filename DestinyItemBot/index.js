0eed1f7d9ef47344a2cc4f65d83d98dd15952fd6
Giuseppe Arienzo <55873294+BeppeTemp@users.noreply.github.com>
2021-01-22 16:56:53 +0100

    miglioramenti

--- DestinyItemBot/index.js
+++ DestinyItemBot/index.js
@@ -1,10 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+// Read environment variables from .env file
 const path = require('path');
-const dotenv = require('dotenv');
 const ENV_FILE = path.join(__dirname, '.env');
-dotenv.config({ path: ENV_FILE });
+require('dotenv').config({
+    path: ENV_FILE
+});
 
 const restify = require('restify');
 
