649f8e0a2ecdc43e1eba8af98cb795f761e72de0
Giuseppe Arienzo <55873294+Tempesta58@users.noreply.github.com>
2021-01-24 21:42:13 +0100

    non ci capisco una fava

--- callFunction/getExample.js
+++ callFunction/getExample.js
@@ -1,16 +1,14 @@
 const axios = require('axios');
 
-let response2 = (async () => {return await axios.get('https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/', 
-    { headers: {
-       'X-API-Key': 'f607ba1906f849f09040a3c4a87e0d32'
-    } } )
+let response2 = (async () => {
+    return await axios.get('https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/', 
+    { 
+        headers: {'X-API-Key': 'f607ba1906f849f09040a3c4a87e0d32'} 
+    })
 })()
 
 response2.then(function(result) {
-
     console.log(result.data.Response.data.inventoryItem.itemName);
-
-
 })
 .catch(function(error) {
     console.log(error);
