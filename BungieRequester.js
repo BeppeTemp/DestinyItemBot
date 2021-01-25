8174f376712cb1531f71b29e2b445af542404ecb
Giuseppe Arienzo <55873294+BeppeTemp@users.noreply.github.com>
2021-01-25 17:56:56 +0100

    la morte

--- BungieRequester.js
+++ BungieRequester.js
@@ -4,6 +4,7 @@ const qs = require("qs");
 const path = require('path');
 const dotenv = require('dotenv');
 const { INVOKE_RESPONSE_KEY } = require("botbuilder");
+const { error } = require("console");
 const ENV_FILE = path.join(__dirname, '.env');
 dotenv.config({ path: ENV_FILE });
 
@@ -19,55 +20,52 @@ class BungieRequester {
         this.apiKey = apiKey;
         this.clientId = clientId;
         this.callBack = callBack;
-    }
 
-    
+        
+    }
 
     get loginlink(){
         var responseType = "response_type=code&";
         var callBackUri = "redirect_uri="+this.callBack+"&";
-        var state = "state=6i0mkLx79Hp91nzWVeHrzHG4";
+        var state = "state=6i0mkLx79Hp91nzWVeHrzHG4"; //Aggiungere controllo
 
         return this.baseLoginPath+responseType+"client_id="+this.clientId+"&"+callBackUri+state;
     }
 
-    accessToken(code){
+    async getAccessData(code){
+        var res = {
+            access_token: null,
+            token_type: null,
+            expires_in: null,
+            membership_id: null
+        }
 
         const data = {
             client_id : this.clientId,
             grant_type : "authorization_code",
             code : code
         }
-        
-        var response = (async () => {
-            return await axios.post(this.basePath +'/app/oauth/token/',qs.stringify(data))
-            })()
-
-            response.then(function(result){
-                console.log(result.response);
-            })
-            .catch(function(error){
-                console.log(error.response);
-            });
-    }
-
-
-    test(){
-        var response = (async () => {
-            return await axios.get(this.basePath + '/Destiny/Manifest/InventoryItem/1274330687/',{ 
-                headers: {'X-API-Key': this.apiKey } 
-            })
-        })()
-        
-        response.then(function(result) {
-            console.log(result.data.Response.data.inventoryItem.itemName);
-        })
-        .catch(function(error) {
-            console.log(error);
+     
+        await axios.post(this.basePath+'/app/oauth/token/', qs.stringify(data))
+        .then(result => {
+            res.access_token = result.data.access_token;
+            res.token_type = result.data.token_type;
+            res.expires_in = result.data.expires_in;
+            res.membership_id = result.data.membership_id;
+        }).catch(error => {
+            console.log(error.data);
         });
+
+        return res;
     }
 }
 
 var br = new BungieRequester(process.env.BungieApiKey, process.env.BungieClientId, process.env.BungieCallBack);
 console.log(br.loginlink);
-br.accessToken("2b65c01bab385109a948b0bc4ebccb68");
+
+async function test(){
+    let a = await br.getAccessData("22476253f0c9c7b9d38af5b387f1fe20");
+    console.log(a.access_token);
+}
+
+test();
