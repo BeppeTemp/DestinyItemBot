953061a73b47c45581b570a43a84a2afabcad51f
Giuseppe Arienzo <55873294+BeppeTemp@users.noreply.github.com>
2021-01-27 09:35:38 +0100

    queue test

--- WebFunction/DIMWebFunction/index.js
+++ WebFunction/DIMWebFunction/index.js
@@ -1,5 +1,4 @@
-const { QueueClient, QueueServiceClient } = require("@azure/storage-queue");
-const qs = require("qs");
+const { QueueServiceClient } = require("@azure/storage-queue");
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -9,11 +8,10 @@ module.exports = async function (context, req) {
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    var codeb = req.query.code;
-    console.log(codeb);
-    codeb = codeb.split(",");
-    console.log(codeb[1]);
-    storecode(qs.stringify(codeb[1]));
+    var codeb = req.query.code.split(",")[1];
+    var state = req.query.state;
+
+    storecode(codeb,state);
 
     context.res = {
         // status: 200, /* Defaults to 200 */
@@ -21,9 +19,17 @@ module.exports = async function (context, req) {
     };
 }
 
-async function storecode(codeb){
+async function storecode(codeb, state){
     const connectionString = "DefaultEndpointsProtocol=https;AccountName=dimstorageaccount;AccountKey=ChV1Qk57/9m3xSnnMbzNC2NI4R3uBsVxRW0gE8fVKb6+5vMhMpwRkOSPcXdrPBCOD+qzvyop+sIgNJKJBCkmCQ==;EndpointSuffix=core.windows.net"
+    const queueName = state.toLowerCase();
+    
     const queueServiceClient = QueueServiceClient.fromConnectionString(connectionString);
-    const queueClient = queueServiceClient.getQueueClient("dimloginqueue");
+    const queueClient = queueServiceClient.getQueueClient(queueName);
+
+    await queueClient.create();
+    
     await queueClient.sendMessage(codeb);
 }
+
+
+//Test link: http://localhost:7071/api/DIMWebFunction?code=2CL9pybUae91yGjtu0W1Ty7mJ6S1jB8tzkZ6S6b1F5KYKuviXaVx2w%3D%3D&code=86f9b04b6c9c2e0fce626133bdf68e84&state=6i0mkLx79Hp91nzWVeHrzHG4
